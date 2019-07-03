3c544d9c3c8d587a1643fe55f739884baf0c279f
Nan <dingnan0701@gmail.com>
2019-07-03 11:13:33 -0700

    update collect and merge

--- collect_metrics.sh
+++ collect_metrics.sh
@@ -1,41 +1,43 @@
 #!/bin/bash
 set -x
-kenrel=(extendSeedLGappedXDropOneDirection)
+kenrel=(transposeNaive transposeCoalesced)
 #timing for kenrels
-srun -n 1 nvprof --print-gpu-summary ./a.out |& tee clean.log 
+#srun -n 1 nvprof --print-gpu-summary ./transpose |& tee clean.log 
 
 #for nvvp 
-#srun -n 1 nvprof --analysis-metrics --output-profile xdrop.nvvp ./a.out  
+#srun -n 1 nvprof --analysis-metrics --output-profile xdrop.nvvp ./transpose  
 for k in ${kenrel[@]}
 do
 	echo "Profiling kernel: ${k}"
 	
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics ipc --metrics inst_executed --metrics inst_integer ./a.out |& tee ${k}_set1.log 
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics inst_compute_ld_st --metrics ldst_executed --metrics ldst_fu_utilization ./a.out |& tee ${k}_ld.log 
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics inst_bit_convert --metrics inst_control ./a.out |& tee ${k}_set2.log 
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics inst_fp_64  --metrics inst_fp_32 --metrics inst_fp_16 ./a.out |& tee ${k}_set3.log 
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics flop_count_dp --metrics flop_count_sp --metrics flop_count_hp ./a.out |& tee ${k}_set4.log 
-    srun -n 1 nvprof --kernels "${k}" --csv --metrics flop_count_dp_fma ./a.out |& tee ${k}_flop_count_dp_fma.log
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics local_load_transactions --metrics local_store_transactions ./a.out |& tee ${k}_local.log 
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics shared_load_transactions --metrics shared_store_transactions  ./a.out |& tee ${k}_share.log  
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics gst_transactions --metrics gld_transactions ./a.out |& tee ${k}_global.log 
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics local_load_transactions_per_request --metrics local_store_transactions_per_request ./main |& tee local_req_${k}.log
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics shared_load_transactions_per_request --metrics shared_store_transactions_per_request ./main |& tee shared_req_${k}.log
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics gld_transactions_per_request --metrics gst_transactions_per_request ./main |& tee gld_req_${k}.log
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics inst_executed_global_reductions --metrics inst_executed_global_stores --metrics inst_executed_global_loads ./main |& tee inst_glo_${k}.log
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics inst_executed_local_loads --metrics inst_executed_local_stores ./main |& tee inst_local_${k}.log
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics inst_executed_shared_loads --metrics inst_executed_shared_stores ./main |& tee inst_shared_${k}.log
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics l2_write_transactions --metrics l2_read_transactions ./a.out |& tee ${k}_l2.log 
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics dram_read_transactions --metrics dram_write_transactions ./a.out |& tee ${k}_dram.log
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics sysmem_read_transactions --metrics sysmem_write_transactions ./a.out |& tee ${k}_sysmem.log
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics branch_efficiency ./a.out |& tee ${k}_branch_efficiency.log
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics warp_nonpred_execution_efficiency --metrics warp_execution_efficiency ./a.out |& tee ${k}_warp_execu_eff.log
-	srun -n 1 nvprof --kernels "${k}" --csv --events inst_executed --events thread_inst_executed ./a.out |& tee ${k}_event.log
-	#srun -n 1 nvprof --kernels "${k}" --csv --events shared_ld_bank_conflict --events shared_st_bank_conflict ./a.out |& tee ${k}_controlflow.log
-    #srun -n 1 nvprof --kernels "${k}" --csv --metrics stall_constant_memory_dependency --metrics stall_exec_dependency --metrics stall_inst_fetch ./a.out |& tee ${k}_stall1.log
-	#srun -n 1 nvprof --kernels "${k}" --csv --metrics stall_memory_dependency --metrics stall_memory_throttle --metrics stall_not_select ./a.out |& tee ${k}_stall2.log
-	#srun -n 1 nvprof --kernels "${k}" --csv --metrics stall_sleeping --metrics stall_pipe_busy --metrics stall_other ./a.out |& tee ${k}_stall3.log
-	#srun -n 1 nvprof --kernels "${k}" --csv --metrics stall_sync ./a.out |& tee ${k}_stall4.log
+	srun -n 1 nvprof --kernels "${k}" --csv --metrics ipc --metrics inst_executed --metrics inst_integer ./transpose |& tee ${k}_set1.log 
+	srun -n 1 nvprof --kernels "${k}" --csv --metrics inst_compute_ld_st --metrics ldst_executed --metrics ldst_fu_utilization ./transpose |& tee ${k}_ld.log 
+	srun -n 1 nvprof --kernels "${k}" --csv --metrics inst_bit_convert --metrics inst_control ./transpose |& tee ${k}_set2.log 
+	srun -n 1 nvprof --kernels "${k}" --csv --metrics inst_fp_64  --metrics inst_fp_32 --metrics inst_fp_16 ./transpose |& tee ${k}_set3.log 
+	srun -n 1 nvprof --kernels "${k}" --csv --metrics flop_count_dp --metrics flop_count_sp --metrics flop_count_hp ./transpose |& tee ${k}_set4.log 
+    srun -n 1 nvprof --kernels "${k}" --csv --metrics flop_count_dp_fma ./transpose |& tee ${k}_flop_count_dp_fma.log
+	srun -n 1 nvprof --kernels "${k}" --csv --metrics local_load_transactions --metrics local_store_transactions ./transpose |& tee ${k}_local.log 
+	srun -n 1 nvprof --kernels "${k}" --csv --metrics shared_load_transactions --metrics shared_store_transactions  ./transpose |& tee ${k}_share.log  
+	srun -n 1 nvprof --kernels "${k}" --csv --metrics gst_transactions --metrics gld_transactions ./transpose |& tee ${k}_global.log 
+	srun -n 1 nvprof --kernels "${k}" --csv --metrics local_load_transactions_per_request --metrics local_store_transactions_per_request ./transpose |& tee ${k}_local_req.log
+	srun -n 1 nvprof --kernels "${k}" --csv --metrics shared_load_transactions_per_request --metrics shared_store_transactions_per_request ./transpose |& tee ${k}_shared_req.log
+	srun -n 1 nvprof --kernels "${k}" --csv --metrics gld_transactions_per_request --metrics gst_transactions_per_request ./transpose |& tee ${k}_gld_req.log
+	srun -n 1 nvprof --kernels "${k}" --csv --metrics inst_executed_global_reductions --metrics inst_executed_global_stores --metrics inst_executed_global_loads ./transpose |& tee ${k}_inst_glo.log
+	srun -n 1 nvprof --kernels "${k}" --csv --metrics inst_executed_local_loads --metrics inst_executed_local_stores ./transpose |& tee ${k}_inst_local.log
+	srun -n 1 nvprof --kernels "${k}" --csv --metrics inst_executed_shared_loads --metrics inst_executed_shared_stores ./transpose |& tee ${k}_inst_shared.log
+	srun -n 1 nvprof --kernels "${k}" --csv --metrics l2_write_transactions --metrics l2_read_transactions ./transpose |& tee ${k}_l2.log 
+	srun -n 1 nvprof --kernels "${k}" --csv --metrics dram_read_transactions --metrics dram_write_transactions ./transpose |& tee ${k}_dram.log
+	srun -n 1 nvprof --kernels "${k}" --csv --metrics sysmem_read_transactions --metrics sysmem_write_transactions ./transpose |& tee ${k}_sysmem.log
+	srun -n 1 nvprof --kernels "${k}" --csv --metrics branch_efficiency ./transpose |& tee ${k}_branch_efficiency.log
+	srun -n 1 nvprof --kernels "${k}" --csv --metrics warp_nonpred_execution_efficiency --metrics warp_execution_efficiency ./transpose |& tee ${k}_warp_execu_eff.log
+	srun -n 1 nvprof --kernels "${k}" --csv --events inst_executed --events thread_inst_executed ./transpose |& tee ${k}_event.log
+	
+    
+    #srun -n 1 nvprof --kernels "${k}" --csv --events shared_ld_bank_conflict --events shared_st_bank_conflict ./transpose |& tee ${k}_controlflow.log
+    #srun -n 1 nvprof --kernels "${k}" --csv --metrics stall_constant_memory_dependency --metrics stall_exec_dependency --metrics stall_inst_fetch ./transpose |& tee ${k}_stall1.log
+	#srun -n 1 nvprof --kernels "${k}" --csv --metrics stall_memory_dependency --metrics stall_memory_throttle --metrics stall_not_select ./transpose |& tee ${k}_stall2.log
+	#srun -n 1 nvprof --kernels "${k}" --csv --metrics stall_sleeping --metrics stall_pipe_busy --metrics stall_other ./transpose |& tee ${k}_stall3.log
+	#srun -n 1 nvprof --kernels "${k}" --csv --metrics stall_sync ./transpose |& tee ${k}_stall4.log
 
 
 
--- merge.sh
+++ merge.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
-kernels=(extendSeedLGappedXDropOneDirectionShared)
-metric=(inst_executed thread_inst_executed inst_integer inst_compute_ld_st ldst_executed inst_fp_64 inst_fp_32 inst_fp_16 flop_count_dp flop_count_sp flop_count_hp flop_count_dp_fma local_load_transactions local_store_transactions local_load_transactions_per_request local_store_transactions_per_request inst_executed_local_loads inst_executed_local_stores shared_load_transactions shared_store_transactions shared_load_transactions_per_request shared_store_transactions_per_request inst_executed_shared_loads inst_executed_shared_stores gst_transactions gld_transactions gld_transactions_per_request gst_transactions_per_request inst_executed_global_reductions inst_executed_global_loads inst_executed_global_stores l2_read_transactions l2_write_transactions dram_read_transactions dram_write_transactions sysmem_read_transactions sysmem_write_transactions ipc) 
+kernels=(transposeNaive transposeCoalesced)
+metric=(inst_integer inst_compute_ld_st ldst_executed inst_fp_64 inst_fp_32 inst_fp_16 flop_count_dp flop_count_sp flop_count_hp flop_count_dp_fma local_load_transactions local_store_transactions local_load_transactions_per_request local_store_transactions_per_request inst_executed_local_loads inst_executed_local_stores shared_load_transactions shared_store_transactions shared_load_transactions_per_request shared_store_transactions_per_request inst_executed_shared_loads inst_executed_shared_stores gst_transactions gld_transactions gld_transactions_per_request gst_transactions_per_request inst_executed_global_reductions inst_executed_global_loads inst_executed_global_stores l2_read_transactions l2_write_transactions dram_read_transactions dram_write_transactions sysmem_read_transactions sysmem_write_transactions ipc) 
+
+event=(inst_executed thread_inst_executed)
+
 for kernel in ${kernels[@]}
 do
 	echo ${kernel}
-	filename="${dir}_${kernel}"
+	filename="${kernel}"
 	echo ${filename}
 	
 	for m in ${metric[@]}
 	do
 		echo $m
-		data=`grep -rin -E "${kernel}.*\"${m}\"" ./${kernel}_*.log | awk -F':' '{print $3}'`  
+		data=`grep -rin -E "${kernel}.*\"${m}\"" ./${kernel}_*.log | awk -F',' '{print $7,$11}'`  
 		echo "${data}" >> ${filename}.csv
-	
 	done
+	
+    for e in ${event[@]}
+	do
+		echo $e
+		data=`grep -rin -E "${kernel}.*\"${e}\"" ./${kernel}_event.log | awk -F',' '{print $7,$10}'`  
+		echo "${data}" >> ${filename}.csv
+	done
+    sed -i "s/ /,/g" ${filename}.csv
 done
