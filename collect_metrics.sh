8f7f69dea51d43729db7856ccc9902d0b01858f9
nanding0701 <42023341+nanding0701@users.noreply.github.com>
2019-10-14 11:43:55 -0700

    Update collect_metrics.sh

--- collect_metrics.sh
+++ collect_metrics.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -x
-kenrel=(transposeNaive transposeCoalesced)
+kernel=(transposeNaive transposeCoalesced)
 #timing for kenrels
 srun -n 1 nvprof --print-gpu-summary ./transpose |& tee clean.log 
 
-for k in ${kenrel[@]}
+for k in ${kernel[@]}
 do
 	echo "Profiling kernel: ${k}"
 	
@@ -25,18 +25,5 @@ do
 	srun -n 1 nvprof --kernels "${k}" --csv --metrics inst_executed_shared_loads --metrics inst_executed_shared_stores ./transpose |& tee ${k}_inst_shared.log
 	srun -n 1 nvprof --kernels "${k}" --csv --metrics l2_write_transactions --metrics l2_read_transactions ./transpose |& tee ${k}_l2.log 
 	srun -n 1 nvprof --kernels "${k}" --csv --metrics dram_read_transactions --metrics dram_write_transactions ./transpose |& tee ${k}_dram.log
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics sysmem_read_transactions --metrics sysmem_write_transactions ./transpose |& tee ${k}_sysmem.log
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics branch_efficiency ./transpose |& tee ${k}_branch_efficiency.log
-	srun -n 1 nvprof --kernels "${k}" --csv --metrics warp_nonpred_execution_efficiency --metrics warp_execution_efficiency ./transpose |& tee ${k}_warp_execu_eff.log
-	srun -n 1 nvprof --kernels "${k}" --csv --events inst_executed --events thread_inst_executed ./transpose |& tee ${k}_event.log
-	
-    
-    #srun -n 1 nvprof --kernels "${k}" --csv --events shared_ld_bank_conflict --events shared_st_bank_conflict ./transpose |& tee ${k}_controlflow.log
-    #srun -n 1 nvprof --kernels "${k}" --csv --metrics stall_constant_memory_dependency --metrics stall_exec_dependency --metrics stall_inst_fetch ./transpose |& tee ${k}_stall1.log
-	#srun -n 1 nvprof --kernels "${k}" --csv --metrics stall_memory_dependency --metrics stall_memory_throttle --metrics stall_not_select ./transpose |& tee ${k}_stall2.log
-	#srun -n 1 nvprof --kernels "${k}" --csv --metrics stall_sleeping --metrics stall_pipe_busy --metrics stall_other ./transpose |& tee ${k}_stall3.log
-	#srun -n 1 nvprof --kernels "${k}" --csv --metrics stall_sync ./transpose |& tee ${k}_stall4.log
-
-
-
+	srun -n 1 nvprof --kernels "${k}" --csv --metrics sysmem_read_transactions --metrics sysmem_write_transactions ./transpose |& tee ${k}_sysmem.log	srun -n 1 nvprof --kernels "${k}" --csv --events inst_executed --events thread_inst_executed ./transpose |& tee ${k}_event.lo
 done
